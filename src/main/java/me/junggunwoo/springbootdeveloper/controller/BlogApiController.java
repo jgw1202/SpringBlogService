faf07aa85e82ac8d1f0b98ad71c15c0bcc31d0ba
jgw1202 <jgw1202@naver.com>
2024-03-13 13:31:53 +0900

    fix : apllication.yml 파일 수정 - 하이버네이트 초기화 전 sql 파일 먼저 실행되는 오류 해결

--- src/main/java/me/junggunwoo/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/junggunwoo/springbootdeveloper/controller/BlogApiController.java
@@ -3,12 +3,16 @@ package me.junggunwoo.springbootdeveloper.controller;
 import lombok.RequiredArgsConstructor;
 import me.junggunwoo.springbootdeveloper.domain.Article;
 import me.junggunwoo.springbootdeveloper.dto.AddArticleRequest;
+import me.junggunwoo.springbootdeveloper.dto.ArticleResponse;
 import me.junggunwoo.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController // HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
@@ -17,11 +21,26 @@ public class BlogApiController {
     private final BlogService blogService;
 
     // HTTP 메소드가 POST 일 때 전달받은 URL과 동일하면 메소드로 매핑
-    @PostMapping("api/articles")
+    @PostMapping("/api/articles")
     // 요청 본문 값 매핑
     public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request){
+
         Article savedArticle = blogService.save(request);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
+    }
+
+    // GET 요청이 오면 글 전체 조회 메소드 호출 후 응답용 객체인 ArticleResponse 로 파싱해
+    // body에 담아 클라이언트에게 전송 (스트림 적용)
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
--- src/main/java/me/junggunwoo/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/junggunwoo/springbootdeveloper/service/BlogService.java
@@ -6,6 +6,8 @@ import me.junggunwoo.springbootdeveloper.dto.AddArticleRequest;
 import me.junggunwoo.springbootdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor // final 이 붙거나 @Notnull 이 붙은 필드의 생성자 추가
 @Service // 빈 으로 등록
 public class BlogService {
@@ -15,5 +17,10 @@ public class BlogService {
     // 블로그 글 추가 메소드
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    // 블로그 글 목록 조회 메소드
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 }
