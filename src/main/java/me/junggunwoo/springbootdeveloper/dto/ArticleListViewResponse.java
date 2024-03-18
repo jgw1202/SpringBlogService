ff7dd5cfd2dee94868cae9904c9cb32611191480
jgw1202 <jgw1202@naver.com>
2024-03-18 16:26:41 +0900

    feat : 타임리프를 이용한 블로그 화면 구성

---
+++ src/main/java/me/junggunwoo/springbootdeveloper/dto/ArticleListViewResponse.java
@@ -0,0 +1,18 @@
+package me.junggunwoo.springbootdeveloper.dto;
+
+import lombok.Getter;
+import me.junggunwoo.springbootdeveloper.domain.Article;
+
+@Getter
+public class ArticleListViewResponse {
+
+    private final Long id;
+    private final String title;
+    private final String content;
+
+    public ArticleListViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
